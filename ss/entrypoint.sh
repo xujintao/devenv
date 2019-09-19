1ab37203a876f3b44dfbb6771ffe3ebbde56d7cb
xujintao <xujintao@126.com>
2019-09-19 23:34:14 +0800

    debug ss entrypoint.sh

--- ss/entrypoint.sh
+++ ss/entrypoint.sh
@@ -23,28 +23,28 @@ done
 #  start ss-client
 #}
 
-if [ -n "$SERVER"]; then
-# start obfs-local
-if [ -n "$OBFS" ]; then
-  obfs-server $OBFS > obfs.out 2>&1 &
-fi
-# start server
-shadowsocks2 \
--s $SERVER \
--verbose
-else if [ -n "$CLIENT"]; then # start client
-# start privoxy
-privoxy /etc/privoxy/config
-# start obfs-local
-if [ -n "$OBFS" ]; then
-  obfs-local $OBFS > obfs.out 2>&1 &
-fi
-# start shadowsocks client
-shadowsocks2 \
--c $CLIENT \
--verbose \
--socks :1080 \
--u \
--udptun :8053=8.8.8.8:53,:8054=8.8.4.4:53 \
--tcptun :8053=8.8.8.8:53,:8054=8.8.4.4:53
+if [ -n "$SERVER" ]; then # start client
+  # start obfs-local
+  if [ -n "$OBFS" ]; then
+    obfs-server $OBFS > obfs.out 2>&1 &
+  fi
+  # start server
+  shadowsocks2 \
+  -s $SERVER \
+  -verbose
+elif [ -n "$CLIENT" ]; then # start client
+  # start privoxy
+  privoxy /etc/privoxy/config
+  # start obfs-local
+  if [ -n "$OBFS" ]; then
+    obfs-local $OBFS > obfs.out 2>&1 &
+  fi
+  # start shadowsocks client
+  shadowsocks2 \
+  -c $CLIENT \
+  -verbose \
+  -socks :1080 \
+  -u \
+  -udptun :8053=8.8.8.8:53,:8054=8.8.4.4:53 \
+  -tcptun :8053=8.8.8.8:53,:8054=8.8.4.4:53
 fi
